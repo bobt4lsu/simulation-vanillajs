d4324793c0a4fd14af530b44398b61417229a920
Bob <bobt4lsu@att.net>
2019-05-09 17:07:42 -0500

    Skills-check 1

---
+++ index.js
@@ -0,0 +1,13 @@
+ const name = document.getElementById('inputName').value;
+ const amount = document.getElementById('inputAmount').value;
+
+ 
+
+ const expense = name + amount; 
+ const button = document.querySelector('button');
+ let ul = document.querySelector('ul');
+
+
+
+
+function expenseList() {}
